abfc5f90af87ca146331c1875ad6692cb991fc7f
Bruno Luz <bsilva.se@gmail.com>
2022-01-13 12:38:02 -0300

    main - Iniciando primeiro microserviço: Matemática

---
+++ game-okk-matematica/src/test/java/dev/bstk/gameokkmatematica/GameOkkMatematicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.gameokkmatematica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOkkMatematicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
