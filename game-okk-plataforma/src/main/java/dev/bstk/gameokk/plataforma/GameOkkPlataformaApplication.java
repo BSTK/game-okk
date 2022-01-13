b73d02e328241ea083007401aefde44d65647694
Bruno Luz <bsilva.se@gmail.com>
2022-01-13 17:25:23 -0300

    main - Microservice de plataforma

---
+++ game-okk-plataforma/src/main/java/dev/bstk/gameokk/plataforma/GameOkkPlataformaApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.gameokk.plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOkkPlataformaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOkkPlataformaApplication.class, args);
+    }
+
+}
