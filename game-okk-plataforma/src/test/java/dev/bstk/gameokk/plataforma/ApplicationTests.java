45da8fdd6546d9b52e79775d73225ee817841147
Bruno Luz <bsilva.se@gmail.com>
2022-01-13 17:28:03 -0300

    main - Refatorando nomeclarura class Application

--- game-okk-plataforma/src/test/java/dev/bstk/gameokk/plataforma/ApplicationTests.java
+++ game-okk-plataforma/src/test/java/dev/bstk/gameokk/plataforma/ApplicationTests.java
@@ -1,13 +1,15 @@
 package dev.bstk.gameokk.plataforma;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GameOkkPlataformaApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> {});
     }
 
 }
