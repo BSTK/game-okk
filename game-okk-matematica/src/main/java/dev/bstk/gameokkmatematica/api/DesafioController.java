b06c54ff9f9e6f7e299a9eb8bdafa9a7a26c8a8f
Bruno Luz <bsilva.se@gmail.com>
2022-03-01 22:09:36 -0300

    main - Matematica: Refatorando métodos controller

--- game-okk-matematica/src/main/java/dev/bstk/gameokkmatematica/api/DesafioController.java
+++ game-okk-matematica/src/main/java/dev/bstk/gameokkmatematica/api/DesafioController.java
@@ -1,28 +1,44 @@
 package dev.bstk.gameokkmatematica.api;
 
 import dev.bstk.gameokk.core.Mapper;
+import dev.bstk.gameokkmatematica.api.request.DesafioTentativaRespostaRequest;
 import dev.bstk.gameokkmatematica.api.response.DesafioResponse;
+import dev.bstk.gameokkmatematica.api.response.DesafioTentativaRespostaResponse;
+import dev.bstk.gameokkmatematica.domain.DesafioTentativaResposta;
 import dev.bstk.gameokkmatematica.domain.service.DesafioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/matematica/desafios")
+@RequestMapping("/api/v1/matematica")
 public class DesafioController {
 
     private final DesafioService desafioService;
 
-    @GetMapping
+    @GetMapping("/desafios")
     public ResponseEntity<DesafioResponse> desafioAleatorio() {
         final var desafio = desafioService.gerarDesafioAleatorio();
         final var desafioResponse = Mapper.to(desafio, DesafioResponse.class);
+
         return ResponseEntity.ok(desafioResponse);
+    }
+
+    @PostMapping("/tentativa-resposta")
+    public ResponseEntity<DesafioTentativaRespostaResponse> tentativaResposta(
+        @RequestBody @Valid final DesafioTentativaRespostaRequest request) {
+        log.info("Recebido uma nova tentativa do usuário: [ {} ]", request.getApelido());
+
+        final DesafioTentativaResposta tentativaResposta = desafioService.tentativaResposta(request);
+        final DesafioTentativaRespostaResponse tentativaRespostaResponse = Mapper
+            .to(tentativaResposta, DesafioTentativaRespostaResponse.class);
+
+        return ResponseEntity.ok(tentativaRespostaResponse);
     }
 
 }
