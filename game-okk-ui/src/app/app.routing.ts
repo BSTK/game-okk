681ccb8ac792991e4fa4bc28a99f6d93b51bc43f
Bruno Luz <bsilva.se@gmail.com>
2022-03-17 12:37:20 -0300

    Ui: Refatorando projeto forntend - Estruturando componente Home

---
+++ game-okk-ui/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import {APP_BASE_HREF} from '@angular/common';
+import {ModuleWithProviders} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module')
+      .then(module => module.HomeModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.module')
+      .then(module => module.HomeModule)
+  }
+];
+
+export const APP_ROUTING_PROVIDER: any[] = [
+  {provide: APP_BASE_HREF, useValue: '/game-okk'}
+];
+
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES, {useHash: true});
