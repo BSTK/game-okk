3694e7a194c79b6e98d18e3ef9e5bef3cf7ac86e
Bruno Luz <bsilva.se@gmail.com>
2022-03-29 20:13:02 -0300

    main - Jogo da Forca: Adicionando teste unitário - ListDicaConverterTest

--- game-okk-jogo-forca/src/test/java/dev/bstk/gameokkjogoforca/domain/model/converter/ListDicaConverterTest.java
+++ game-okk-jogo-forca/src/test/java/dev/bstk/gameokkjogoforca/domain/model/converter/ListDicaConverterTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 class ListDicaConverterTest {
 
@@ -16,8 +17,8 @@ class ListDicaConverterTest {
     private final ListDicaConverter converter = new ListDicaConverter();
 
     @Test
-    @DisplayName("Deve converter uma lista de dicas num JSON valido")
-    void deveConverterUmaListaDeDicasNumJSONValido() throws JsonProcessingException {
+    @DisplayName("Deve converter uma lista de dicas em uma string JSON valida")
+    void deveConverterUmaListaDeDicasEmUmaStringJSONValida() throws JsonProcessingException {
         final var dicas = TesteHelper.fixure("/fixture/forca/palavra-secreta-dicas.json", Dica[].class);
         final var dicasJsonEsperado = objectMapper.writeValueAsString(dicas);
 
@@ -28,5 +29,20 @@ class ListDicaConverterTest {
             .isNotEmpty()
             .isNotBlank()
             .isEqualTo(dicasJsonEsperado);
+    }
+
+    @Test
+    @DisplayName("Deve converter uma string JSON em uma lista de Dicas valida")
+    void deveConverterUmaStringJSONListaDicasEmUmaListaDeDicasValida() throws JsonProcessingException {
+        final var dicas = TesteHelper.fixure("/fixture/forca/palavra-secreta-dicas.json", Dica[].class);
+        final var dicasJson = objectMapper.writeValueAsString(dicas);
+
+        List<Dica> dicasParseada = converter.convertToEntityAttribute(dicasJson);
+
+        Assertions.assertThat(dicasParseada)
+            .isNotNull()
+            .isNotEmpty()
+            .hasSize(3)
+            .hasOnlyElementsOfType(Dica.class);
     }
 }
